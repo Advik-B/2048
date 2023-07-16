6149bd819cb9e3f9ec91a761ae9629157dead994
Advik <advik.b@gmail.com>
2023-07-16 17:08:37 +0530

    Update matrix_logic.py

--- matrix_logic.py
+++ matrix_logic.py
@@ -1,122 +1,104 @@
-# from grid_helper import Grid
-# from grid_helper import Grid
-
+import random
 
 class GameLogic:
+    def __init__(
+        self,
+        matrix: list[
+            list[int, int, int, int],
+            list[int, int, int, int],
+            list[int, int, int, int],
+            list[int, int, int, int]
+        ]
+    ) -> None:
+        self.matrix = matrix
 
-    def __init__(self, grid: "Grid"):
-        self.g_matrix = grid.matrix
-        self.g_size = grid.size
-
-    def move_left(self):
-        # Move all the tiles to the left
-        # Example:
+    def is_full(self) -> bool:
         """
-        [2, 2, 2, 2] -> [4, 4, 0, 0]
-        [2, 0, 2, 2] -> [4, 2, 0, 0]
-        [2, 0, 0, 2] -> [4, 0, 0, 0]
-        [0, 0, 0, 2] -> [2, 0, 0, 0]
-
-        Merging is done if and ONLY if the tiles are the same
+        Check if the game board is full.
         """
-        # First, move all the tiles to the left
-        for i in range(self.g_size[0]):
-            for j in range(self.g_size[1]):
-                if self.g_matrix[i][j] == 0:
-                    for k in range(j + 1, self.g_size[1]):
-                        if self.g_matrix[i][k] != 0:
-                            self.g_matrix[i][j] = self.g_matrix[i][k]
-                            self.g_matrix[i][k] = 0
-                            break
+        for row in self.matrix:
+            if 0 in row:
+                return False
+        return True
 
-        # Now, merge the tiles
-        for i in range(self.g_size[0]):
-            for j in range(self.g_size[1] - 1):
-                if self.g_matrix[i][j] == self.g_matrix[i][j + 1]:
-                    self.g_matrix[i][j] *= 2
-                    self.g_matrix[i][j + 1] = 0
-
-
-    def move_right(self):
-        # Move all the tiles to the right
-        # Example:
+    def place_random_number(self) -> None:
         """
-        [2, 2, 2, 2] -> [0, 0, 4, 4]
-        [2, 0, 2, 2] -> [0, 0, 2, 4]
-        [2, 0, 0, 2] -> [0, 0, 0, 4]
-        [0, 0, 0, 2] -> [0, 0, 0, 2]
-
-        Merging is done if and ONLY if the tiles are the same
+        Place a random number (2 or 4) at an empty location on the board.
         """
-        # First, move all the tiles to the right
-        for i in range(self.g_size[0]):
-            for j in range(self.g_size[1] - 1, -1, -1):
-                if self.g_matrix[i][j] == 0:
-                    for k in range(j - 1, -1, -1):
-                        if self.g_matrix[i][k] != 0:
-                            self.g_matrix[i][j] = self.g_matrix[i][k]
-                            self.g_matrix[i][k] = 0
-                            break
+        if self.is_full():
+            return
 
-        # Now, merge the tiles
-        for i in range(self.g_size[0]):
-            for j in range(self.g_size[1] - 1, 0, -1):
-                if self.g_matrix[i][j] == self.g_matrix[i][j - 1]:
-                    self.g_matrix[i][j] *= 2
-                    self.g_matrix[i][j - 1] = 0
+        while True:
+            row = random.randint(0, 3)
+            col = random.randint(0, 3)
+            if self.matrix[row][col] == 0:
+                self.matrix[row][col] = random.choice([2, 4])
+                break
 
-    def move_up(self):
-        # Move all the tiles to the up
-        # Example:
+    def move_left(self) -> None:
         """
-        [2, 2, 2, 2] -> [4, 4, 0, 0]
-        [2, 0, 2, 2] -> [4, 2, 2, 0]
-        [2, 0, 0, 2] -> [4, 0, 0, 2]
-        [0, 0, 0, 2] -> [2, 0, 0, 0]
-
-        Merging is done if and ONLY if the tiles are the same
+        Move the numbers to the left and merge adjacent numbers if they are equal.
         """
-        # First, move all the tiles to the up
-        for i in range(self.g_size[0]):
-            for j in range(self.g_size[1]):
-                if self.g_matrix[j][i] == 0:
-                    for k in range(j + 1, self.g_size[1]):
-                        if self.g_matrix[k][i] != 0:
-                            self.g_matrix[j][i] = self.g_matrix[k][i]
-                            self.g_matrix[k][i] = 0
-                            break
+        for row in self.matrix:
+            # Merge numbers if they are equal
+            for i in range(3):
+                if row[i] == row[i + 1] and row[i] != 0:
+                    row[i] *= 2
+                    row[i + 1] = 0
 
-        # Now, merge the tiles
-        for i in range(self.g_size[0]):
-            for j in range(self.g_size[1] - 1):
-                if self.g_matrix[j][i] == self.g_matrix[j + 1][i]:
-                    self.g_matrix[j][i] *= 2
-                    self.g_matrix[j + 1][i] = 0
+            # Shift numbers to the left
+            temp_row = [num for num in row if num != 0]  # Remove zeros
+            temp_row += [0] * (4 - len(temp_row))  # Pad with zeros
+            row[:] = temp_row
 
-    def move_down(self):
-        # Move all the tiles to the down
-        # Example:
+    def move_right(self) -> None:
         """
-        [2, 2, 2, 2] -> [0, 0, 0, 2]
-        [2, 0, 2, 2] -> [0, 0, 2, 4]
-        [2, 0, 0, 2] -> [0, 0, 2, 0]
-        [0, 0, 0, 2] -> [0, 0, 0, 2]
-
-        Merging is done if and ONLY if the tiles are the same
+        Move the numbers to the right and merge adjacent numbers if they are equal.
         """
-        # First, move all the tiles to the down
-        for i in range(self.g_size[0]):
-            for j in range(self.g_size[1] - 1, -1, -1):
-                if self.g_matrix[j][i] == 0:
-                    for k in range(j - 1, -1, -1):
-                        if self.g_matrix[k][i] != 0:
-                            self.g_matrix[j][i] = self.g_matrix[k][i]
-                            self.g_matrix[k][i] = 0
-                            break
+        # Reverse each row, move left, and reverse back
+        for row in self.matrix:
+            row.reverse()
+        self.move_left()
+        for row in self.matrix:
+            row.reverse()
 
-        # Now, merge the tiles
-        for i in range(self.g_size[0]):
-            for j in range(self.g_size[1] - 1, 0, -1):
-                if self.g_matrix[j][i] == self.g_matrix[j - 1][i]:
-                    self.g_matrix[j][i] *= 2
-                    self.g_matrix[j - 1][i] = 0
+    def move_up(self) -> None:
+        """
+        Move the numbers up and merge adjacent numbers if they are equal.
+        """
+        # Transpose the matrix, move left, and transpose back
+        self.transpose()
+        self.move_left()
+        self.transpose()
+
+    def move_down(self) -> None:
+        """
+        Move the numbers down and merge adjacent numbers if they are equal.
+        """
+        # Transpose the matrix, move right, and transpose back
+        self.transpose()
+        self.move_right()
+        self.transpose()
+
+    def transpose(self) -> None:
+        """
+        Transpose the matrix (rows become columns and vice versa).
+        """
+        self.matrix = [[self.matrix[j][i] for j in range(4)] for i in range(4)]
+
+    def display(self) -> None:
+        """
+        Display the current state of the game board.
+        """
+        for row in self.matrix:
+            for num in row:
+                print(num, end="\t")
+            print()
+
+    def reset(self) -> None:
+        """
+        Reset the game board to its initial state.
+        """
+        for i in range(4):
+            for j in range(4):
+                self.matrix[i][j] = 0
