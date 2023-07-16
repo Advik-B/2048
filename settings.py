8bfab61d1052733a94c4684dde895d41d8babd28
Advik <advik.b@gmail.com>
2023-07-16 19:58:24 +0530

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,3 +1,17 @@
+from pygame.locals import (
+    K_ESCAPE,
+    K_r,
+    K_m,
+    K_UP,
+    K_DOWN,
+    K_LEFT,
+    K_RIGHT,
+    K_w,
+    K_s,
+    K_a,
+    K_d,
+)
+
 DISPLAY_SIZE = (600, 800)
 VSYNC = True
 ALLOW_RESIZE = False
@@ -5,3 +19,18 @@ FULLSCREEN = False
 
 WINDOW_TITLE = '2048 - by Advik, for Pratibha'
 FPS = 60
+
+# Colors
+BACKGROUND_COLOR = (250, 248, 239)
+GRID_COLOR = (187, 173, 160)
+TEXT_COLOR = (119, 110, 101)
+
+# Key bindings
+KEY_BINDINGS = {
+    "reset": [K_r],
+    "mute": [K_m],
+    "up": [K_UP, K_w],
+    "down": [K_DOWN, K_s],
+    "left": [K_LEFT, K_a],
+    "right": [K_RIGHT, K_d],
+}
