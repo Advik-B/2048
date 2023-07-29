84b1b8ab838a8d75a4e62196a1ec6e9ec91e1bce
Advik <advik.b@gmail.com>
2023-07-29 23:41:13 +0530

    Update console_edition.py

--- console_edition.py
+++ console_edition.py
@@ -44,7 +44,7 @@ while True:
         game.move_right()
     elif move == "a":
         result = game.autoplay(priority="space")
-        print(f"Autoplay: {result[-1]}")
+        print(f"Autoplay: {result}")
     elif move == "h":
         help_msg()
         continue
