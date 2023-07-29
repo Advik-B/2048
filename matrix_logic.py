23061d9e6402b2e708a7359c67dab3cf64e1a3ba
Advik <advik.b@gmail.com>
2023-07-29 23:04:11 +0530

    Update matrix_logic.py

--- matrix_logic.py
+++ matrix_logic.py
@@ -28,7 +28,7 @@ class GameLogic:
         Save the current state of the game board to the state stack.
         """
         self.state_stack.append(deepcopy(self.matrix))
-        if 0 < self.max_undo < len(self.state_stack):
+        if self.max_undo > 0 and len(self.state_stack) > self.max_undo:
             self.state_stack.pop(0)  # Remove the oldest state
 
     def undo(self) -> None:
@@ -56,7 +56,7 @@ class GameLogic:
 
         return self.matrix
 
-    def spawn_customised(self, chance_of_numbers:dict):
+    def spawn_customised(self, chance_of_numbers: dict) -> None | list[list[int, int, int, int]]:
         """
         Place a random number (given by chance_of_numbers) at an empty location on the board.
         :param chance_of_numbers: {2: 90, 4: 10}, {2: 80, 4: 20}, {2: 70, 4: 20, 8: 10} etc.
@@ -157,3 +157,132 @@ class GameLogic:
             self.matrix[i][j] = 0
 
         return self.matrix
+
+    def calculate_smoothness(self) -> float:
+        """
+        Calculate the smoothness score for the current board state.
+        """
+        smoothness_score = 0
+
+        for row in self.matrix:
+            for col in range(3):
+                if row[col] != 0 and row[col + 1] != 0:
+                    smoothness_score -= abs(row[col] - row[col + 1])
+
+        for col in range(4):
+            for row in range(3):
+                if self.matrix[row][col] != 0 and self.matrix[row + 1][col] != 0:
+                    smoothness_score -= abs(self.matrix[row][col] - self.matrix[row + 1][col])
+
+        return smoothness_score
+
+    def calculate_monotonicity(self) -> float:
+        """
+        Calculate the monotonicity score for the current board state.
+        """
+        monotonicity_score = 0
+
+        for row in self.matrix:
+            if all(row[i] >= row[i + 1] for i in range(3)):
+                monotonicity_score += sum(row[i] - row[i + 1] for i in range(3))
+            elif all(row[i] <= row[i + 1] for i in range(3)):
+                monotonicity_score += sum(row[i + 1] - row[i] for i in range(3))
+
+        for col in range(4):
+            column_values = [self.matrix[row][col] for row in range(4)]
+            if all(column_values[i] >= column_values[i + 1] for i in range(3)):
+                monotonicity_score += sum(column_values[i] - column_values[i + 1] for i in range(3))
+            elif all(column_values[i] <= column_values[i + 1] for i in range(3)):
+                monotonicity_score += sum(column_values[i + 1] - column_values[i] for i in range(3))
+
+        return monotonicity_score
+
+    def autoplay(self, priority: str = "score"):
+        """
+        AI Autoplay function to make the best move based on the current matrix information.
+        :param priority: The aspect to prioritize - "score" or "space".
+        """
+        if priority not in ["score", "space"]:
+            raise ValueError("Invalid priority. Use 'score' or 'space'.")
+
+        possible_moves = ['left', 'right', 'up', 'down']
+        max_score = float('-inf')
+        best_move = None
+
+        # Save the current state before making any moves
+        self.save_state()
+
+        for move in possible_moves:
+            # Make the move and get the resulting matrix
+            if move == 'left':
+                self.move_left()
+            elif move == 'right':
+                self.move_right()
+            elif move == 'up':
+                self.move_up()
+            elif move == 'down':
+                self.move_down()
+
+            # Calculate the score for this move using the advanced heuristic function
+            score = self.calculate_heuristic_score(priority)
+
+            # Undo the move to revert the matrix back to the original state
+            self.undo()
+
+            # Update the best move if the score is higher
+            if score > max_score:
+                max_score = score
+                best_move = move
+
+        # Make the best move
+        if best_move == 'left':
+            self.move_left()
+        elif best_move == 'right':
+            self.move_right()
+        elif best_move == 'up':
+            self.move_up()
+        elif best_move == 'down':
+            self.move_down()
+
+        return self.matrix, best_move
+
+    def calculate_heuristic_score(self, priority: str) -> float:
+        """
+        Calculate the heuristic score for the current board state.
+        :param priority: The aspect to prioritize - "score" or "space".
+        """
+        # Factor weights for each heuristic component
+        smoothness_weight = 0.1
+        monotonicity_weight = 1.0
+        empty_cells_weight = 2.7
+        max_tile_weight = 1.0
+
+        # Additional weights based on the chosen priority
+        if priority == "score":
+            score_weight = 1.0
+            space_weight = 0.0
+        elif priority == "space":
+            score_weight = 0.0
+            space_weight = 1.0
+
+        # Smoothness score
+        smoothness_score = self.calculate_smoothness()
+
+        # Monotonicity score
+        monotonicity_score = self.calculate_monotonicity()
+
+        # Empty cells score
+        empty_cells_score = len([(row, col) for row in range(4) for col in range(4) if self.matrix[row][col] == 0])
+
+        # Max tile score
+        max_tile_score = max(max(row) for row in self.matrix)
+
+        return (
+            smoothness_weight * smoothness_score
+            + monotonicity_weight * monotonicity_score
+            + empty_cells_weight * empty_cells_score
+            + max_tile_weight * max_tile_score
+            + score_weight * max_tile_score
+            + space_weight  # Add score_weight component based on priority
+            * empty_cells_score  # Add space_weight component based on priority
+        )
